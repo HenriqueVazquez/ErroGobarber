a0906546e00a75c829bcb700fe5de057e4e1e57f
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2022-02-28 18:29:51 -0300

    erro templates

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd0dd567b534935',
+    pass: '887407a3c1ae89',
+  },
+  default: {
+    from: 'Equipe GoBarber <norply@gobarber.com>',
+  },
+};
